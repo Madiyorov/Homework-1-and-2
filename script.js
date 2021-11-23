e97ed083ae3a05861f4afd3e2d1e7aebc6a542f6
Islom <b.madiyorov.i@mail.ru>
2021-11-23 16:12:24 +0500

    JavaScript

---
+++ script.js
@@ -0,0 +1,35 @@
+
+let name = prompt('Kак вас зовут?');
+
+let lastname = prompt('Kак ваша фамилия?');
+
+alert('Рад быль познокомится ' + name + ' ' + lastname);
+
+var str = +prompt('Введите своё имя');
+
+var str = +prompt('Введите свой возраст');
+
+var srt = +prompt('10 + 18 = ?' )
+console.log(srt);
+
+console.log( '10 + 18 = ' + (10 + 18) + ' Ваш ответ: 28' );
+
+var srt = +prompt('38 - 10 = ?' )
+console.log(srt);
+
+console.log( '38 - 10 = ' + (38 - 10) + ' Ваш ответ: 28' );
+
+var srt = +prompt('1 * 28 = ?' )
+console.log(srt);
+
+console.log( '1 * 28 = ' + (1 * 28) + ' Ваш ответ: 28' );
+
+var srt = +prompt('28 / 1 = ?' )
+console.log(srt);
+
+console.log( '28 / 1 = ' + (28 / 1) + ' Ваш ответ: 28' );
+
+var srt = +prompt('28 % 100 = ?' )
+console.log(srt);
+
+console.log( '28 % 100 = ' + (28 % 100) + ' Ваш ответ: 28' );
